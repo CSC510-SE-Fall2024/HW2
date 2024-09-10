392729e952682f6ce560c180172167769ea6d41e
Atharva Pansare <61797592+Atharva7007@users.noreply.github.com>
2024-09-10 21:15:57 +0000

    Changed mergeSort to merge_sort

--- test_hw2_debugging.py
+++ test_hw2_debugging.py
@@ -10,24 +10,24 @@ def test_empty_array():
     """
     Test case for an empty array.
 
-    Asserts that calling mergeSort on an empty list returns an empty list.
+    Asserts that calling merge_sort on an empty list returns an empty list.
     """
-    assert hw2_debugging.mergeSort([]) == []
+    assert hw2_debugging.merge_sort([]) == []
 
 def test_sorted_array():
     """
     Test case for an already sorted array.
 
-    Asserts that calling mergeSort on a list that is already sorted returns the same list.
+    Asserts that calling merge_sort on a list that is already sorted returns the same list.
     """
-    assert hw2_debugging.mergeSort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    assert hw2_debugging.merge_sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
 
 def test_random_array():
     """
     Test case for a random unsorted array.
 
-    Asserts that calling mergeSort on a randomly ordered list returns a correctly sorted list.
+    Asserts that calling merge_sort on a randomly ordered list returns a correctly sorted list.
     """
     arr = [10, 2, 14, 3, 7]
     sorted_arr = [2, 3, 7, 10, 14]
-    assert hw2_debugging.mergeSort(arr) == sorted_arr
+    assert hw2_debugging.merge_sort(arr) == sorted_arr
